6788cb866f58e6848ae1d3f5ab50951df0b1db07
Sriya Balineni <33444116+sriyabalineni@users.noreply.github.com>
2022-04-14 22:54:40 -0700

    Add files via upload

---
+++ pet.py
@@ -0,0 +1,8 @@
+import pymongo
+  
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+mydb = myclient["mydatabase"]
+dblist = myclient.list_database_names()
+if "mydatabase" in dblist:
+  print("The database exists.")
